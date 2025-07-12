031d27bd28d36f411218041b0cda50672c983abb
Moneesh12 <168181257+Moneesh12@users.noreply.github.com>
2025-07-12 09:06:23 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,8 +1,9 @@
+#for finding the range of a list
 def find_range(num):
     if len(num)<3:
-        return "Range determination is not possible"
+        return "Range determination is not possible"#if number of elements less than 3 then print this
     else:
-        r = max(num) - min(num)
+        r = max(num) - min(num)#we are finding the maximum element of the list and subtracting with minimum to get the range
     return r
 
 l = find_range([0,4,6,7,8,12])
