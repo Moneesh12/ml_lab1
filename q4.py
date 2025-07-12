7d6eccd4daf9593e51820d795dd44899941ab806
Moneesh12 <168181257+Moneesh12@users.noreply.github.com>
2025-07-12 09:07:12 +0530

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,14 +1,17 @@
+#for finding the most repeated letter in a word
 word = "hippopatamus"
 
-l = list(word)
+l = list(word)#converts the word into a list of letters
 
 max_ch = ''
 maxcount = 0
 
 for i in range(len(l)):
     a = l[i]
-    count1 = l.count(a)
-    
+    count1 = l.count(a)#counts how many times the letter repeated
+
+
+#used for checking if the current count is more than the current maximum    
     if count1>maxcount:
         maxcount = count1
         max_ch = a
