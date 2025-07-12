0424dd2fb886039c6ac4ce4ef01bb8953572c83f
Moneesh12 <168181257+Moneesh12@users.noreply.github.com>
2025-07-12 09:20:01 +0530

    Update q5.py

--- q5.py
+++ q5.py
@@ -5,7 +5,7 @@ import random
 
 list1 = [random.randint(1,10) for _ in range(25)]#this generates a list from 1 to 10 with 25 elements
 print(list1)
-sum = 0#used for finding sum of a list
+sum = 0
 for i in range(len(list1)):
     #we are finding the sum of the array and dividing by the number of elements to get mean
     sum = sum + list1[i]
@@ -32,9 +32,9 @@ list2 = sorted(list1)
 n = len(list2)
 
 if n%2==0:
-    median = (list2[n//2-1] + list2[n//2])/2#we are using the median formula when number of elements are even
+    median = (list2[n//2-1] + list2[n//2])/2#median formula when number of elements are even
 else:
-    median = list2[n//2]#we are using the median formula when number of elements are odd
+    median = list2[n//2]#median formula when number of elements are odd
 
 print("median:",median)
 
